7c8870c5d54e4312f1fb8602f22f3a287c621f0b
fe-luna <luna.chenzhang@gmail.com>
2023-06-02 14:16:14 +0800

    feat: emotion cache

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import createCache from "@emotion/cache";
+import { CacheProvider } from "@emotion/react";
 import { ChakraBaseProvider } from "@chakra-ui/react";
 import { todoistTheme } from "styles/chakra-ui";
 import Layout from "layouts";
@@ -10,6 +12,12 @@ import Project from "pages/project";
 import Projects from "pages/projects";
 import Today from "pages/today";
 import Upcoming from "pages/upcoming";
+
+// prepending the chakra-ui styles, then CSS Modules can overwrite the CSS-in-JS
+const emotionCache = createCache({
+  key: "emotion-cache",
+  prepend: true,
+});
 
 function App() {
   const router = createBrowserRouter([
@@ -34,9 +42,11 @@ function App() {
   ]);
 
   return (
-    <ChakraBaseProvider theme={todoistTheme}>
-      <RouterProvider router={router} />
-    </ChakraBaseProvider>
+    <CacheProvider value={emotionCache}>
+      <ChakraBaseProvider theme={todoistTheme}>
+        <RouterProvider router={router} />
+      </ChakraBaseProvider>
+    </CacheProvider>
   );
 }
 
